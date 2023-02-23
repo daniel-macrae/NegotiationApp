c869cbf91c134bf5dc9046e68a4e33420de9cd5b
daniel-macrae <d.macrae@student.rug.nl>
2023-02-23 16:12:19 +0100

    Initial Commit

---
+++ Negotiation/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Negotiation
+//
+//  Created by CogModel on 23/02/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ Negotiation/NegotiationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NegotiationApp.swift
+//  Negotiation
+//
+//  Created by CogModel on 23/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct NegotiationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
